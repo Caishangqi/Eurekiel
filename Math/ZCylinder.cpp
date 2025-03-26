8aebef18b0a272c16057f846f486b15dd72fa11e
CaIzii <2470159730@qq.com>
2025-03-26 03:17:38 -0500

    Math-ZCylinder: introduce Z axis Cylinder.

---
+++ Math/ZCylinder.cpp
@@ -0,0 +1,34 @@
+#include "ZCylinder.hpp"
+
+ZCylinder::ZCylinder()
+{
+}
+
+ZCylinder::~ZCylinder()
+{
+}
+
+ZCylinder::ZCylinder(const ZCylinder& copyFrom)
+{
+    m_center = copyFrom.m_center;
+    m_height = copyFrom.m_height;
+    m_radius = copyFrom.m_radius;
+}
+
+ZCylinder::ZCylinder(const Vec3& center, float radius, float height): m_center(center), m_height(height), m_radius(radius)
+{
+}
+
+ZCylinder::ZCylinder(const Vec3& centerOrBase, float radius, float height, bool isBasePosition)
+    : m_radius(radius), m_height(height)
+{
+    if (isBasePosition)
+    {
+        m_center = centerOrBase + Vec3(0.f, 0.f, height / 2.0f);
+    }
+    else
+    {
+        // 否则直接认为传入的就是中心位置
+        m_center = centerOrBase;
+    }
+}
---
+++ Math/ZCylinder.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "Vec3.hpp"
+
+class ZCylinder
+{
+public:
+    ZCylinder();
+    ~ZCylinder();
+
+    ZCylinder(const ZCylinder& copyFrom);
+    explicit ZCylinder(const Vec3& center, float radius, float height);
+    ZCylinder(const Vec3& centerOrBase, float radius, float height, bool isBasePosition);
+
+public:
+    Vec3  m_center = Vec3::ZERO;
+    float m_height = 0.f;
+    float m_radius = 0.f;
+};
