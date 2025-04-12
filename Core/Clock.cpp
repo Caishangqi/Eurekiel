9334df95ccae69419d7754f479072c96f9e53f45
CaIzii <2470159730@qq.com>
2025-04-12 14:54:35 -0500

    Engine/Core: Introduce Clock.

---
+++ Core/Clock.cpp
@@ -0,0 +1,138 @@
+﻿#include "Clock.hpp"
+
+#include "Time.hpp"
+Clock* Clock::g_theSystemClock = new Clock();
+
+Clock::Clock()
+{
+}
+
+Clock::Clock(Clock& parent): m_parent(&parent)
+{
+    if (m_parent)
+        m_parent->AddChild(this);
+}
+
+Clock::~Clock()
+{
+    for (Clock* m_child : m_children)
+    {
+        delete m_child;
+        m_child = nullptr;
+    }
+}
+
+void Clock::Reset()
+{
+}
+
+bool Clock::IsPaused() const
+{
+    return m_isPaused;
+}
+
+void Clock::Pause()
+{
+    m_isPaused = true;
+}
+
+void Clock::Unpause()
+{
+    m_isPaused = false;
+}
+
+void Clock::TogglePause()
+{
+}
+
+void Clock::StepSingleFrame()
+{
+    m_stepSingleFrame = true;
+}
+
+void Clock::SetTimeScale(float timeScale)
+{
+    m_timeScale = timeScale;
+}
+
+float Clock::GetTimeScale() const
+{
+    return m_timeScale;
+}
+
+float Clock::GetDeltaSeconds() const
+{
+    return m_deltaSeconds;
+}
+
+float Clock::GetTotalSeconds() const
+{
+    return m_totalSeconds;
+}
+
+float Clock::GetFrameRate() const
+{
+    if (m_deltaSeconds > 0.0)
+        return 1.0f / m_deltaSeconds;
+    return 0.0;
+}
+
+int Clock::GetFrameCount() const
+{
+    return m_frameCount;
+}
+
+Clock& Clock::GetSystemClock()
+{
+    return *g_theSystemClock;
+}
+
+void Clock::TickSystemClock()
+{
+    GetSystemClock().Tick();
+}
+
+void Clock::Tick()
+{
+    float timeNow = static_cast<float>(GetCurrentTimeSeconds());
+    Advance(timeNow - m_lastUpdateTimeInSeconds);
+}
+
+void Clock::Advance(float deltaTimeSeconds)
+{
+    if (m_isPaused && !m_stepSingleFrame)
+    {
+        m_deltaSeconds            = 0.0f;
+        m_lastUpdateTimeInSeconds = static_cast<float>(GetCurrentTimeSeconds());
+        return;
+    }
+
+    if (m_stepSingleFrame)
+    {
+        m_stepSingleFrame = false;
+    }
+
+    float scaledDelta = deltaTimeSeconds * m_timeScale;
+    m_frameCount++;
+    m_deltaSeconds = scaledDelta;
+    m_totalSeconds += scaledDelta;
+    m_lastUpdateTimeInSeconds += deltaTimeSeconds;
+    for (Clock* child : m_children)
+    {
+        child->Advance(scaledDelta);
+    }
+}
+
+void Clock::AddChild(Clock* childClock)
+{
+    m_children.push_back(childClock);
+}
+
+void Clock::RemoveChild(Clock* childClock)
+{
+    auto it = std::find(m_children.begin(), m_children.end(), childClock);
+    if (it != m_children.end())
+    {
+        m_children.erase(it);
+    }
+}
---
+++ Core/Clock.hpp
@@ -0,0 +1,75 @@
+﻿#pragma once
+
+#pragma once
+#include <vector>
+
+// Hierarchical clock that inherits time scale.
+// Parent clocks pass scaled delta seconds down to child clocks as their base delta seconds.
+// Child clocks in turn scale that time and pass it down to their children.
+// There is one system clock at the root of the hierarchy.
+class Clock
+{
+public:
+    // Default constructor, uses the system clock as the parent of the new clock.
+    Clock();
+
+    // Constructor to specify a parent clock for the new clock.
+    explicit Clock(Clock& parent);
+
+    // Destructor, Un-parents itself and its children to avoid crashes but does not otherwise fix the hierarchy.
+    ~Clock();
+    Clock(const Clock& copy) = delete;
+
+    // Reset all bookkeeping variables back to zero and set the last updated time to the current system time.
+    void Reset();
+
+    bool IsPaused() const;
+    void Pause();
+    void Unpause();
+    void TogglePause();
+
+    // Unpause for one frame, then pause again.
+    void StepSingleFrame();
+
+    // Set and get the time scale value for this clock.
+    void  SetTimeScale(float timeScale);
+    float GetTimeScale() const;
+
+    float GetDeltaSeconds() const;
+    float GetTotalSeconds() const;
+    float GetFrameRate() const;
+    int   GetFrameCount() const;
+
+    // Get a reference to the static system clock that acts as the default parent clock.
+    static Clock& GetSystemClock();
+
+    // Tick the system clock, updating the entire hierarchy.
+    static void TickSystemClock();
+
+protected:
+    // Tick function to calculate delta seconds and update time.
+    void Tick();
+
+    // Advance the clock, updating bookkeeping variables and propagating changes to child clocks.
+    void Advance(float deltaTimeSeconds);
+
+    // Manage child clocks.
+    void AddChild(Clock* childClock);
+    void RemoveChild(Clock* childClock);
+
+    Clock*              m_parent = nullptr; // Parent clock, nullptr for root clock.
+    std::vector<Clock*> m_children; // Child clocks.
+
+    // Bookkeeping variables.
+    float m_lastUpdateTimeInSeconds = 0.0;
+    float m_totalSeconds            = 0.0;
+    float m_deltaSeconds            = 0.0;
+    int   m_frameCount              = 0;
+
+    float m_timeScale       = 1.0; // Time scale factor.
+    bool  m_isPaused        = false; // Pause state.
+    bool  m_stepSingleFrame = false; // Single frame step mode.
+    float m_maxDeltaSeconds = 0.1f; // Maximum delta time for debugging.
+
+    static Clock* g_theSystemClock;
+};
