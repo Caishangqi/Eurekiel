eb1be3d64363acf498f9d0091533726f99caba78
CaIzii <2470159730@qq.com>
2025-05-05 02:19:56 -0500

    Math/AABB3: Add func `GetCorners()` and rewrite RayCast Method.

--- Math/AABB3.cpp
+++ Math/AABB3.cpp
@@ -2,6 +2,8 @@
 
 #include <algorithm>
 
+#include "Plane3.hpp"
+
 AABB3::~AABB3()
 {
 }
@@ -13,6 +15,23 @@ bool AABB3::IsPointInside(const Vec3& point) const
         (point.z >= m_mins.z && point.z <= m_maxs.z);
 }
 
+std::vector<Vec3> AABB3::GetCorners() const
+{
+    std::vector<Vec3> corners;
+    Vec3              dimensions = m_maxs - m_mins;
+    corners.reserve(8);
+    corners.emplace_back(m_mins.x, m_mins.y, m_mins.z + dimensions.z);
+    corners.emplace_back(m_mins.x, m_mins.y, m_mins.z);
+    corners.emplace_back(m_mins.x, m_mins.y + dimensions.y, m_mins.z);
+    corners.emplace_back(m_mins.x, m_mins.y + dimensions.y, m_mins.z + dimensions.z);
+
+    corners.emplace_back(m_mins.x + dimensions.x, m_mins.y, m_mins.z + dimensions.z);
+    corners.emplace_back(m_mins.x + dimensions.x, m_mins.y, m_mins.z);
+    corners.emplace_back(m_mins.x + dimensions.x, m_mins.y + dimensions.y, m_mins.z);
+    corners.emplace_back(m_mins.x + dimensions.x, m_mins.y + dimensions.y, m_mins.z + dimensions.z);
+    return corners;
+}
+
 const Vec3 AABB3::GetCenter() const
 {
     return m_mins + ((m_maxs - m_mins) / 2);
@@ -21,6 +40,118 @@ const Vec3 AABB3::GetCenter() const
 const Vec3 AABB3::GetDimensions() const
 {
     return m_maxs - m_mins;
+}
+
+bool AABB3::IsOverlapping(const Plane3& other) const
+{
+    return IsOverlapping(*this, other);
+}
+
+RaycastResult3D AABB3::Raycast(Vec3 startPos, Vec3 fwdNormal, float maxDist) const
+{
+    return AABB3::Raycast(startPos, fwdNormal, maxDist, *this);
+}
+
+bool AABB3::IsOverlapping(const AABB3& aabb3, const Plane3& other)
+{
+    int numOfFront = 0;
+    int numOfBack  = 0;
+    for (Vec3& corner : aabb3.GetCorners())
+    {
+        other.IsPointInFrontOfPlane(corner) ? numOfFront += 1 : numOfBack += 1;
+    }
+    if (numOfBack == 0 && numOfFront == 8)
+    {
+        return false;
+    }
+    if (numOfFront == 0 && numOfBack == 8)
+    {
+        return false;
+    }
+    return true;
+}
+
+RaycastResult3D AABB3::Raycast(Vec3 startPos, Vec3 fwdNormal, float maxDist, const AABB3& aabb3)
+{
+    RaycastResult3D result;
+    result.m_didImpact    = false;
+    result.m_rayStartPos  = startPos;
+    result.m_rayFwdNormal = fwdNormal;
+    result.m_rayMaxLength = maxDist;
+
+    // If the starting point is inside the AABB3, return collision immediately
+    if (startPos.x >= aabb3.m_mins.x && startPos.x <= aabb3.m_maxs.x &&
+        startPos.y >= aabb3.m_mins.y && startPos.y <= aabb3.m_maxs.y &&
+        startPos.z >= aabb3.m_mins.z && startPos.z <= aabb3.m_maxs.z)
+    {
+        result.m_didImpact    = true;
+        result.m_impactDist   = 0.f;
+        result.m_impactPos    = startPos;
+        result.m_impactNormal = -fwdNormal;
+        return result;
+    }
+
+    // Perform slab test in world space
+    constexpr float EPS  = 1e-6f;
+    float           tMin = 0.f;
+    float           tMax = maxDist;
+
+    float tEntry[3] = {0.f, 0.f, 0.f};
+    // Perform slab intersection test on each axis
+    float minB[3] = {aabb3.m_mins.x, aabb3.m_mins.y, aabb3.m_mins.z};
+    float maxB[3] = {aabb3.m_maxs.x, aabb3.m_maxs.y, aabb3.m_maxs.z};
+    float orig[3] = {startPos.x, startPos.y, startPos.z};
+    float dir[3]  = {fwdNormal.x, fwdNormal.y, fwdNormal.z};
+
+    for (int axis = 0; axis < 3; axis++)
+    {
+        if (fabsf(dir[axis]) < EPS)
+        {
+            if (orig[axis] < minB[axis] || orig[axis] > maxB[axis])
+                return result; // Parallel and starting point is outside
+        }
+        else
+        {
+            float invD = 1.f / dir[axis];
+            float t1   = (minB[axis] - orig[axis]) * invD;
+            float t2   = (maxB[axis] - orig[axis]) * invD;
+            if (t1 > t2) std::swap(t1, t2);
+            if (t2 < tMin || t1 > tMax)
+                return result;
+
+            if (t1 > tMin)
+            {
+                tMin      = t1;
+                tEntry[0] = (axis == 0) ? t1 : tEntry[0];
+                tEntry[1] = (axis == 1) ? t1 : tEntry[1];
+                tEntry[2] = (axis == 2) ? t1 : tEntry[2];
+            }
+            if (t2 < tMax) tMax = t2;
+        }
+    }
+
+    // If the intersection point is not within the valid range
+    if (tMin < 0.f || tMin > maxDist)
+        return result;
+
+    // Fill in the result
+    result.m_didImpact  = true;
+    result.m_impactDist = tMin;
+    result.m_impactPos  = startPos + fwdNormal * tMin;
+
+    // Determine which side was hit
+    constexpr float EPS_T = 1e-4f;
+    Vec3            normalLocal(0.f, 0.f, 0.f);
+    if (fabsf(tEntry[0] - tMin) < EPS_T && fabsf(fwdNormal.x) > EPS)
+        normalLocal.x = (fwdNormal.x > 0.f) ? -1.f : 1.f;
+    else if (fabsf(tEntry[1] - tMin) < EPS_T && fabsf(fwdNormal.y) > EPS)
+        normalLocal.y = (fwdNormal.y > 0.f) ? -1.f : 1.f;
+    else
+        normalLocal.z = (fwdNormal.z > 0.f) ? -1.f : 1.f;
+
+    result.m_impactNormal = normalLocal;
+
+    return result;
 }
 
 void AABB3::Translate(const Vec3& translationToApply)
--- Math/AABB3.hpp
+++ Math/AABB3.hpp
@@ -1,5 +1,10 @@
 ﻿#pragma once
+#include <vector>
+
+#include "RaycastUtils.hpp"
 #include "Vec3.hpp"
+
+class Plane3;
 
 class AABB3
 {
@@ -14,16 +19,22 @@ public:
     }
 
     // Accessors (const methods)
-    bool       IsPointInside(const Vec3& point) const;
-    const Vec3 GetCenter() const;
-    const Vec3 GetDimensions() const;
+    bool              IsPointInside(const Vec3& point) const;
+    std::vector<Vec3> GetCorners() const;
+    const Vec3        GetCenter() const;
+    const Vec3        GetDimensions() const;
+    bool              IsOverlapping(const Plane3& other) const;
+    RaycastResult3D   Raycast(Vec3 startPos, Vec3 fwdNormal, float maxDist) const;
+
+    static bool            IsOverlapping(const AABB3& aabb3, const Plane3& other);
+    static RaycastResult3D Raycast(Vec3 startPos, Vec3 fwdNormal, float maxDist, const AABB3& aabb3);
 
     // Mutators (non-const methods)
     void Translate(const Vec3& translationToApply);
     void SetCenter(const Vec3& newCenter);
     void SetDimensions(const Vec3& newDimensions);
     void StretchToIncludePoint(const Vec3& point); // does minimal stretching required (none if already on point)
-    
+
     AABB3(const AABB3& copyFrom);
     explicit AABB3(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
     explicit AABB3(const Vec3& mins, const Vec3& maxs);
