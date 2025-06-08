ed23648afe6a3bcec32d19821ee1e3f02c493bca
CaIzii <2470159730@qq.com>
2025-06-08 02:27:06 -0500

    Engine/Render: Introduce OpenGL render implementation.

---
+++ Renderer/API/GLRenderer.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Engine/Renderer/IRenderer.hpp"
+
+class GLRenderer : public IRenderer
+{
+public:
+    GLRenderer();
+    void Startup() override;
+    void Shutdown() override;
+    void BeginFrame() override;
+    void EndFrame() override;
+    void ClearScreen(const Rgba8& color) override;
+    void DrawVertexArray(const std::vector<Vertex_PCUTBN>& vertices) override;
+};
