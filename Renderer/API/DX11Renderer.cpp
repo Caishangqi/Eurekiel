fa7d2ff5e0d9ef3f1ae42f100b374ae32702c6bb
CaIzii <2470159730@qq.com>
2025-06-08 02:27:13 -0500

    Engine/Render: Introduce DirectX11 render implementation.

---
+++ Renderer/API/DX11Renderer.cpp
@@ -0,0 +1,1 @@
+﻿#include "DX11Renderer.hpp"
---
+++ Renderer/API/DX11Renderer.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Engine/Renderer/IRenderer.hpp"
+
+class DX11Renderer : public IRenderer
+{
+public:
+    DX11Renderer();
+    void Startup() override;
+    void Shutdown() override;
+    void BeginFrame() override;
+    void EndFrame() override;
+    void ClearScreen(const Rgba8& color) override;
+    void DrawVertexArray(const std::vector<Vertex_PCUTBN>& vertices) override;
+};
