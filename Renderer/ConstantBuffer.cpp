f20dacd562013b5accda41c08047675f495c38f5
CaIzii <2470159730@qq.com>
2025-06-08 02:29:54 -0500

    Engine/Render: Add DirectX12 specific buffer ptr.

--- Renderer/ConstantBuffer.cpp
+++ Renderer/ConstantBuffer.cpp
@@ -1,12 +1,18 @@
 ﻿#include "ConstantBuffer.hpp"
 
+#include <d3d12.h>
+
 #include "Renderer.hpp"
 
 ConstantBuffer::ConstantBuffer(size_t size): m_size(size)
 {
+    m_constantBufferView = new D3D12_CONSTANT_BUFFER_VIEW_DESC();
 }
 
 ConstantBuffer::~ConstantBuffer()
 {
     DX_SAFE_RELEASE(m_buffer)
+    
+    DX_SAFE_RELEASE(m_dx12ConstantBuffer)
+    delete m_constantBufferView;
 }
--- Renderer/ConstantBuffer.hpp
+++ Renderer/ConstantBuffer.hpp
@@ -1,10 +1,13 @@
 ﻿#pragma once
 
 struct ID3D11Buffer;
+struct ID3D12Resource;
+struct D3D12_CONSTANT_BUFFER_VIEW_DESC;
 
 class ConstantBuffer
 {
     friend class Renderer;
+    friend class DX12Renderer;
 
 public:
     ConstantBuffer(size_t size);
@@ -14,4 +17,7 @@ public:
 private:
     ID3D11Buffer* m_buffer = nullptr;
     size_t        m_size   = 0;
+
+    ID3D12Resource*                  m_dx12ConstantBuffer = nullptr;
+    D3D12_CONSTANT_BUFFER_VIEW_DESC* m_constantBufferView = nullptr;
 };
