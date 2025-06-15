9ef95bee475e2031b4d8d078a2ceed5c4f2bb3c9
CaIzii <2470159730@qq.com>
2025-06-14 22:55:59 -0500

    Engine/Render: Add DirectX12 VertexBuffer support.

--- Renderer/VertexBuffer.cpp
+++ Renderer/VertexBuffer.cpp
@@ -1,5 +1,7 @@
 ﻿#include "VertexBuffer.hpp"
 
+#include <ThirdParty/d3dx12/d3dx12.h>
+
 #include "Renderer.hpp"
 #include "Engine/Core/ErrorWarningAssert.hpp"
 
@@ -8,24 +10,66 @@ VertexBuffer::VertexBuffer(ID3D11Device* device, unsigned int size, unsigned int
     Create();
 }
 
+VertexBuffer::VertexBuffer(ID3D12Device* device, unsigned int size, unsigned int stride): m_dx12device(device), m_size(size), m_stride(stride)
+{
+    m_vertexBufferView = new D3D12_VERTEX_BUFFER_VIEW();
+    Create();
+}
+
 VertexBuffer::~VertexBuffer()
 {
     DX_SAFE_RELEASE(m_buffer)
+    DX_SAFE_RELEASE(m_dx12buffer)
+    delete m_vertexBufferView;
 }
 
 void VertexBuffer::Create()
 {
-    UINT              vertexBufferSize = m_size;
-    D3D11_BUFFER_DESC bufferDesc       = {};
-    bufferDesc.Usage                   = D3D11_USAGE_DYNAMIC;
-    bufferDesc.ByteWidth               = vertexBufferSize;
-    bufferDesc.BindFlags               = D3D11_BIND_VERTEX_BUFFER;
-    bufferDesc.CPUAccessFlags          = D3D11_CPU_ACCESS_WRITE;
-
-    HRESULT hr = m_device->CreateBuffer(&bufferDesc, nullptr, &m_buffer);
-    if (!SUCCEEDED(hr))
+    if (m_device)
     {
-        ERROR_AND_DIE("Could not create vertex buffer.")
+        UINT              vertexBufferSize = m_size;
+        D3D11_BUFFER_DESC bufferDesc       = {};
+        bufferDesc.Usage                   = D3D11_USAGE_DYNAMIC;
+        bufferDesc.ByteWidth               = vertexBufferSize;
+        bufferDesc.BindFlags               = D3D11_BIND_VERTEX_BUFFER;
+        bufferDesc.CPUAccessFlags          = D3D11_CPU_ACCESS_WRITE;
+
+        HRESULT hr = m_device->CreateBuffer(&bufferDesc, nullptr, &m_buffer);
+        if (!SUCCEEDED(hr))
+        {
+            ERROR_AND_DIE("Could not create vertex buffer.")
+        }
+        return;
+    }
+    if (m_dx12device)
+    {
+        // Create commited resource for vertex buffer
+        CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_UPLOAD);
+        CD3DX12_RESOURCE_DESC   resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(m_size);
+
+        /// The step you create resource in DirectX12
+        /// 1. You create the big heap on GPU (Chunk of memory)
+        /// 2. Once you have the big chunk of memory you can choose where to place all
+
+        /// Commited resource is a special case
+        /// Direct 3D will create a heap of the same size for you for that resource
+        /// and it will tie it to the resource. And it called implicity heap (you can not see it) because
+        /// it all managed for you behind scenes by Direct3D
+        HRESULT hr = m_dx12device->CreateCommittedResource(
+            &heapProps, // Create heap (need to know the things that describe the heap)
+            D3D12_HEAP_FLAG_NONE,
+            &resourceDesc, // Create Also resource ( need to know the things that describe the resource
+            D3D12_RESOURCE_STATE_GENERIC_READ, // What is the state of the resource after we created it
+            nullptr,
+            IID_PPV_ARGS(&m_dx12buffer));
+        GUARANTEE_OR_DIE(SUCCEEDED(hr), "DX12: CreateVertexBuffer failed")
+
+        /// Create the view for vertex buffer
+        {
+            m_vertexBufferView->BufferLocation = m_dx12buffer->GetGPUVirtualAddress(); // Where is the buffer located in GPU memory
+            m_vertexBufferView->SizeInBytes    = static_cast<UINT>(m_size);
+            m_vertexBufferView->StrideInBytes  = m_stride; // the distance between vertices
+        }
     }
 }
 
--- Renderer/VertexBuffer.hpp
+++ Renderer/VertexBuffer.hpp
@@ -1,18 +1,21 @@
 ﻿#pragma once
 #include <d3d11.h>
+#include <d3d12.h>
 
 struct ID3D11Device;
 
 class VertexBuffer
 {
     friend class Renderer;
-
+    friend class DX12Renderer;
 public:
     VertexBuffer(ID3D11Device* device, unsigned int size, unsigned int stride);
+    VertexBuffer(ID3D12Device* device, unsigned int size, unsigned int stride);
     VertexBuffer(const VertexBuffer& copy) = delete;
     virtual ~VertexBuffer();
 
     void Create();
+    
     void Resize(unsigned int size);
 
     unsigned int GetSize();
@@ -23,4 +26,8 @@ private:
     ID3D11Buffer* m_buffer = nullptr;
     unsigned int  m_size   = 0;
     unsigned int  m_stride = 0;
+
+    ID3D12Device*             m_dx12device       = nullptr;
+    ID3D12Resource*           m_dx12buffer = nullptr; // DX12
+    D3D12_VERTEX_BUFFER_VIEW* m_vertexBufferView = nullptr; // DX12 Create the vertex Buffer view, pure cpu sides unlike descriptor heap
 };
