7b9d17f21c26e05923ccf5024c7f9bd3b965904f
CaIzii <2470159730@qq.com>
2025-06-14 23:01:09 -0500

    Engine/GraphicError: Introduce a global graphic checker that handles `HRESELT`

---
+++ Renderer/GraphicsError.cpp
@@ -0,0 +1,11 @@
+﻿#include "GraphicsError.hpp"
+
+#include "API/DX12Renderer.hpp"
+#include "Engine/Core/ErrorWarningAssert.hpp"
+#include "Engine/Core/StringUtils.hpp"
+
+CheckerToken chk;
+
+HResultGrabber::HResultGrabber(unsigned int hr) noexcept: hr(hr)
+{
+}
---
+++ Renderer/GraphicsError.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <ostream>
+
+#include "API/DX12Renderer.hpp"
+#include "Engine/Core/EngineCommon.hpp"
+#include "Engine/Core/ErrorWarningAssert.hpp"
+#include "Engine/Core/StringUtils.hpp"
+
+struct CheckerToken
+{
+};
+
+extern CheckerToken chk;
+
+struct HResultGrabber
+{
+    HResultGrabber() = default;
+    HResultGrabber(unsigned int hr) noexcept;
+    long hr;
+};
+
+inline void operator>>(HResultGrabber grabber, CheckerToken token)
+{
+    UNUSED(token)
+    if (FAILED(grabber.hr))
+    {
+        ERROR_AND_DIE(Stringf("HRESULT failed with error code %l", grabber.hr))
+    }
+}
