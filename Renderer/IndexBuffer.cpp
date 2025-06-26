099e9977973af0559c1e98005a78f91d013c40a8
CaIzii <2470159730@qq.com>
2025-06-26 00:21:30 -0500

    Engine/IndexBuffer: Implement the DirectX12 backend IndexBuffer.

--- Renderer/IndexBuffer.cpp
+++ Renderer/IndexBuffer.cpp
@@ -1,9 +1,17 @@
 ﻿#include "IndexBuffer.hpp"
 
+#include <ThirdParty/d3dx12/d3dx12.h>
+
+#include "GraphicsError.hpp"
 #include "Renderer.hpp"
 #include "Engine/Core/ErrorWarningAssert.hpp"
 
-IndexBuffer::IndexBuffer(ID3D11Device* device, unsigned int size): m_device(device), m_size(size), m_buffer(nullptr)
+IndexBuffer::IndexBuffer(ID3D11Device* device, unsigned int size): m_buffer(nullptr), m_device(device), m_size(size)
+{
+    Create();
+}
+
+IndexBuffer::IndexBuffer(ID3D12Device* device, unsigned int size): m_buffer(nullptr), m_dx12device(device), m_size(size)
 {
     Create();
 }
@@ -11,21 +19,51 @@ IndexBuffer::IndexBuffer(ID3D11Device* device, unsigned int size): m_device(devi
 IndexBuffer::~IndexBuffer()
 {
     DX_SAFE_RELEASE(m_buffer)
+    DX_SAFE_RELEASE(m_dx12buffer)
 }
 
 void IndexBuffer::Create()
 {
-    UINT              indexBufferSize = m_size;
-    D3D11_BUFFER_DESC bufferDesc      = {};
-    bufferDesc.Usage                  = D3D11_USAGE_DYNAMIC;
-    bufferDesc.ByteWidth              = indexBufferSize;
-    bufferDesc.BindFlags              = D3D11_BIND_INDEX_BUFFER;
-    bufferDesc.CPUAccessFlags         = D3D11_CPU_ACCESS_WRITE;
-
-    HRESULT hr = m_device->CreateBuffer(&bufferDesc, nullptr, &m_buffer);
-    if (!SUCCEEDED(hr))
+    if (m_device)
     {
-        ERROR_AND_DIE("Could not create index buffer.")
+        UINT              indexBufferSize = m_size;
+        D3D11_BUFFER_DESC bufferDesc      = {};
+        bufferDesc.Usage                  = D3D11_USAGE_DYNAMIC;
+        bufferDesc.ByteWidth              = indexBufferSize;
+        bufferDesc.BindFlags              = D3D11_BIND_INDEX_BUFFER;
+        bufferDesc.CPUAccessFlags         = D3D11_CPU_ACCESS_WRITE;
+
+        HRESULT hr = m_device->CreateBuffer(&bufferDesc, nullptr, &m_buffer);
+        if (!SUCCEEDED(hr))
+        {
+            ERROR_AND_DIE("Could not create index buffer.")
+        }
+        return;
+    }
+    // Create commited resource for index buffer
+    if (m_dx12device)
+    {
+        CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_UPLOAD);
+        CD3DX12_RESOURCE_DESC   resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(m_size);
+
+        m_dx12device->CreateCommittedResource(
+            &heapProps,
+            D3D12_HEAP_FLAG_NONE, &resourceDesc,
+            D3D12_RESOURCE_STATE_GENERIC_READ,
+            nullptr,
+            IID_PPV_ARGS(&m_dx12buffer)
+        ) >> chk;
+        /// Create the view for index buffer
+        {
+            m_indexBufferView.BufferLocation = m_dx12buffer->GetGPUVirtualAddress();
+            m_indexBufferView.SizeInBytes    = m_size;
+            // Need be R32 not R16 because we use unsigned int which is 4 byte - 32bit
+            m_indexBufferView.Format = DXGI_FORMAT_R32_UINT; // R is stand by red channel
+            /// R32_UINT        R       32-bit          unsigned int    4B
+            /// R32G32_FLOAT    R + G   32-bit each     float           8B
+            /// R16_UINT        R       16-bit  	    unsigned int    2B
+            /// R8G8B8A8_UNORM  RGBA    8-bit each      normalized      4B
+        }
     }
 }
 
@@ -47,9 +85,22 @@ void IndexBuffer::Update(const unsigned int* indices, unsigned int count, ID3D11
     deviceContext->Unmap(m_buffer, 0);
 }
 
+void IndexBuffer::Update(const unsigned int* indices, unsigned int count, ID3D12Device* deviceContext)
+{
+    UNUSED(deviceContext)
+    unsigned int dataSize = count * sizeof(unsigned int);
+    if (dataSize > m_size) { Resize(dataSize); }
+    CD3DX12_RANGE rng(0, 0);
+    uint8_t*      dst = nullptr;
+    m_dx12buffer->Map(0, &rng, reinterpret_cast<void**>(&dst)) >> chk;
+    memcpy(dst, indices, dataSize);
+    m_dx12buffer->Unmap(0, nullptr);
+}
+
 void IndexBuffer::Resize(unsigned int size)
 {
     DX_SAFE_RELEASE(m_buffer)
+    DX_SAFE_RELEASE(m_dx12buffer)
     m_size = size;
     Create();
 }
@@ -66,5 +117,5 @@ unsigned int IndexBuffer::GetStride()
 
 unsigned int IndexBuffer::GetCount()
 {
-    return int(m_size / sizeof(unsigned int));
+    return static_cast<int>(m_size / sizeof(unsigned int));
 }
--- Renderer/IndexBuffer.hpp
+++ Renderer/IndexBuffer.hpp
@@ -4,18 +4,21 @@
 class IndexBuffer
 {
     friend class Renderer;
+    friend class DX12Renderer;
 
 public:
     IndexBuffer(ID3D11Device* device, unsigned int size);
+    IndexBuffer(ID3D12Device* device, unsigned int size);
     IndexBuffer(const IndexBuffer& copy) = delete;
     virtual ~IndexBuffer();
 
     void Create();
-    /// Deprecated
+    /// Deprecated, should not use in DirectX12 API
     /// @param indices 
     /// @param count 
-    /// @param deviceContext 
+    /// @param deviceContext
     void Update(const unsigned int* indices, unsigned int count, ID3D11DeviceContext* deviceContext);
+    void Update(const unsigned int* indices, unsigned int count, ID3D12Device* deviceContext = nullptr);
     void Resize(unsigned int size);
 
     unsigned int GetSize();
@@ -25,5 +28,9 @@ public:
 private:
     ID3D11Buffer* m_buffer;
     ID3D11Device* m_device = nullptr;
-    unsigned int  m_size   = 0;
+
+    ID3D12Device*           m_dx12device = nullptr;
+    ID3D12Resource*         m_dx12buffer = nullptr; // DX12
+    D3D12_INDEX_BUFFER_VIEW m_indexBufferView;
+    unsigned int            m_size = 0;
 };
