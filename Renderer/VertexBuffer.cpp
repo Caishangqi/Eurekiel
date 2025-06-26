37aa404e3402c7c22959c196c4adea2306353955
CaIzii <2470159730@qq.com>
2025-06-26 00:25:30 -0500

    Engine/VertexBuffer: Implement Frame ring vertex buffer which support multiple drawcall in single `Render()` func.

--- Renderer/VertexBuffer.cpp
+++ Renderer/VertexBuffer.cpp
@@ -2,6 +2,7 @@
 
 #include <ThirdParty/d3dx12/d3dx12.h>
 
+#include "GraphicsError.hpp"
 #include "Renderer.hpp"
 #include "Engine/Core/ErrorWarningAssert.hpp"
 
@@ -10,9 +11,8 @@ VertexBuffer::VertexBuffer(ID3D11Device* device, unsigned int size, unsigned int
     Create();
 }
 
-VertexBuffer::VertexBuffer(ID3D12Device* device, unsigned int size, unsigned int stride): m_dx12device(device), m_size(size), m_stride(stride)
+VertexBuffer::VertexBuffer(ID3D12Device* device, unsigned int size, unsigned int stride): m_size(size), m_stride(stride), m_dx12device(device)
 {
-    m_vertexBufferView = new D3D12_VERTEX_BUFFER_VIEW();
     Create();
 }
 
@@ -20,7 +20,6 @@ VertexBuffer::~VertexBuffer()
 {
     DX_SAFE_RELEASE(m_buffer)
     DX_SAFE_RELEASE(m_dx12buffer)
-    delete m_vertexBufferView;
 }
 
 void VertexBuffer::Create()
@@ -41,7 +40,7 @@ void VertexBuffer::Create()
         }
         return;
     }
-    if (m_dx12device)
+    else if (m_dx12device)
     {
         // Create commited resource for vertex buffer
         CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_UPLOAD);
@@ -64,18 +63,26 @@ void VertexBuffer::Create()
             IID_PPV_ARGS(&m_dx12buffer));
         GUARANTEE_OR_DIE(SUCCEEDED(hr), "DX12: CreateVertexBuffer failed")
 
+        /// Map the buffer to GPU and save the CPU pointer
+        m_dx12buffer->Map(0, nullptr, reinterpret_cast<void**>(&m_cpuPtr)) >> chk;
+
         /// Create the view for vertex buffer
         {
-            m_vertexBufferView->BufferLocation = m_dx12buffer->GetGPUVirtualAddress(); // Where is the buffer located in GPU memory
-            m_vertexBufferView->SizeInBytes    = static_cast<UINT>(m_size);
-            m_vertexBufferView->StrideInBytes  = m_stride; // the distance between vertices
+            m_vertexBufferView.BufferLocation = m_dx12buffer->GetGPUVirtualAddress(); // Where is the buffer located in GPU memory
+            m_vertexBufferView.SizeInBytes    = m_size;
+            m_vertexBufferView.StrideInBytes  = m_stride; // the distance between vertices
         }
+    }
+    else
+    {
+        ERROR_AND_DIE("No render device specify.")
     }
 }
 
 void VertexBuffer::Resize(unsigned int size)
 {
     DX_SAFE_RELEASE(m_buffer)
+    DX_SAFE_RELEASE(m_dx12buffer)
     m_size = size;
     Create();
 }
@@ -88,4 +95,47 @@ unsigned int VertexBuffer::GetSize()
 unsigned int VertexBuffer::GetStride()
 {
     return m_stride;
+}
+
+void VertexBuffer::ResetCursor()
+{
+    m_cursor                          = 0;
+    m_vertexBufferView.BufferLocation = m_dx12buffer->GetGPUVirtualAddress();
+}
+
+bool VertexBuffer::Allocate(const void* scr, size_t size)
+{
+    size_t aligned = AlignUp<size_t>(size, 16);
+    if (m_cursor + aligned > m_size)
+    {
+        ERROR_AND_DIE("Exceed the Vertex buffer max amount")
+    }
+
+    /// The reason why m_cpuPtr + m_cursor can be used to locate a specific byte address in the buffer is mainly based on
+    /// the following points
+    ///
+    /// 1. The type of m_cpuPtr is a byte pointer:
+    /// m_cpuPtr is defined as uint8_t* (or BYTE*), which is a "byte pointer" VertexBuffer. C/C++ stipulates that pointer
+    /// addition offsets the memory address according to the size of the element type pointed to by the pointer. Because
+    /// the element type here is 1 byte (uint8_t), m_cpuPtr + N moves the address backward by N bytes.
+    ///
+    /// 2. m_cursor records the number of bytes currently written
+    /// In Allocate, each call first calculates the number of aligned bytes to be written according to the alignment rule,
+    /// and then accumulates this value in m_cursor. In this way, m_cursor always represents the byte offset of the next
+    /// writable block relative to the beginning of the buffer.
+    ///
+    /// 3. memcpy(m_cpuPtr + m_cursor, scr, aligned) is to copy data from the beginning of the buffer plus the byte offset
+    /// - m_cpuPtr points to the starting location where the GPU upload heap is mapped into the CPU address space.
+    /// - m_cpuPtr + m_cursor points to the place where the current frame has not been filled with data.
+    ///
+    /// Because the pointer type is one byte wide, adding the number of bytes of m_cursor can accurately move to the target
+    /// memory location, thereby realizing the cyclic filling of the "ring" buffer and multiple DrawCalls.
+    memcpy(m_cpuPtr + m_cursor, scr, aligned);
+
+    m_vertexBufferView.BufferLocation = m_vertexBufferView.BufferLocation + m_cursor;
+    m_vertexBufferView.SizeInBytes    = (UINT)size; // the view size increase but the actual buffer size not change.
+    m_vertexBufferView.StrideInBytes  = m_stride;
+
+    m_cursor += aligned;
+    return true;
 }
--- Renderer/VertexBuffer.hpp
+++ Renderer/VertexBuffer.hpp
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <cstdint>
 #include <d3d11.h>
 #include <d3d12.h>
 
@@ -8,6 +9,7 @@ class VertexBuffer
 {
     friend class Renderer;
     friend class DX12Renderer;
+
 public:
     VertexBuffer(ID3D11Device* device, unsigned int size, unsigned int stride);
     VertexBuffer(ID3D12Device* device, unsigned int size, unsigned int stride);
@@ -15,19 +17,26 @@ public:
     virtual ~VertexBuffer();
 
     void Create();
-    
+
     void Resize(unsigned int size);
 
     unsigned int GetSize();
     unsigned int GetStride();
 
+    /// DirectX12
+    void ResetCursor();
+    bool Allocate(const void* scr, size_t size);
+
 private:
     ID3D11Device* m_device = nullptr;
     ID3D11Buffer* m_buffer = nullptr;
-    unsigned int  m_size   = 0;
-    unsigned int  m_stride = 0;
 
-    ID3D12Device*             m_dx12device       = nullptr;
-    ID3D12Resource*           m_dx12buffer = nullptr; // DX12
-    D3D12_VERTEX_BUFFER_VIEW* m_vertexBufferView = nullptr; // DX12 Create the vertex Buffer view, pure cpu sides unlike descriptor heap
+    unsigned int m_size   = 0;
+    unsigned int m_stride = 0;
+
+    uint8_t*                 m_cpuPtr     = nullptr;
+    size_t                   m_cursor     = 0;
+    ID3D12Device*            m_dx12device = nullptr;
+    ID3D12Resource*          m_dx12buffer = nullptr; // DX12
+    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView; // DX12 Create the vertex Buffer view, pure cpu sides unlike descriptor heap
 };
