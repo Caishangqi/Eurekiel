64ce962e8a504483fead527dd2065e79c142841f
CaIzii <2470159730@qq.com>
2025-06-26 00:23:11 -0500

    Engine/Light: Introduce Point light and spot light class and realated funcs.

---
+++ Renderer/Light/Light.cpp
@@ -0,0 +1,54 @@
+﻿#include "Light.hpp"
+
+#include "Engine/Core/Rgba8.hpp"
+#include "Engine/Math/MathUtils.hpp"
+
+Light& Light::SetPosition(Vec3 position)
+{
+    m_position = position;
+    return *this;
+}
+
+Light& Light::SetDirection(Vec3 direction)
+{
+    m_direction = direction.GetNormalized();
+    return *this;
+}
+
+Light& Light::SetColor(Vec4 color)
+{
+    m_color = color;
+    return *this;
+}
+
+Light& Light::SetColor(Rgba8 color)
+{
+    float colorInFloat[4];
+    color.GetAsFloats(colorInFloat);
+    m_color = Vec4(colorInFloat[0], colorInFloat[1], colorInFloat[2], colorInFloat[3]);
+    return *this;
+}
+
+Light& Light::SetInnerRadius(float radius)
+{
+    m_innerRadius = radius;
+    return *this;
+}
+
+Light& Light::SetOuterRadius(float radius)
+{
+    m_outerRadius = radius;
+    return *this;
+}
+
+Light& Light::SetInnerAngle(float angle)
+{
+    m_innerPenumbra = CosDegrees(angle);
+    return *this;
+}
+
+Light& Light::SetOuterAngle(float angle)
+{
+    m_outerPenumbra = CosDegrees(angle);
+    return *this;
+}
---
+++ Renderer/Light/Light.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "Engine/Math/Vec3.hpp"
+#include "Engine/Math/Vec4.hpp"
+
+struct Rgba8;
+
+
+class Light
+{
+public:
+    Light& SetPosition(Vec3 position);
+    Light& SetDirection(Vec3 direction);
+    Light& SetColor(Vec4 color);
+    Light& SetColor(Rgba8 color);
+    Light& SetInnerRadius(float radius);
+    Light& SetOuterRadius(float radius);
+    Light& SetInnerAngle(float angle);
+    Light& SetOuterAngle(float angle);
+
+private:
+    Vec3 m_position = Vec3::ZERO;
+    [[maybe_unused]]
+    float m_pad0      = 0.0f;
+    Vec3  m_direction = Vec3::ZERO;
+    [[maybe_unused]]
+    float m_pad1 = 0.0f;
+    Vec4  m_color;
+    float m_innerPenumbra{0.f};
+    float m_outerPenumbra{0.f};
+    float m_innerRadius{0.f};
+    float m_outerRadius{0.f};
+};
