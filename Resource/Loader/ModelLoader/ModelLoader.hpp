9a8fbb30651044945c7dcb431a529b7f2d540013
CaIzii <2470159730@qq.com>
2025-07-02 21:12:28 -0500

    Engine/ModelLoader: Introduce Base class of ModelLoader and temporary `FMesh` structure.

---
+++ Resource/Loader/ModelLoader/ModelLoader.hpp
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "Engine/Core/Vertex_PCU.hpp"
+#include "Engine/Resource/Loader/Loader.hpp"
+
+struct Vec2;
+struct Vec3;
+class Renderer;
+
+// TODO: Hack the simple Mesh, need refactor in the future
+struct FMesh
+{
+    // Temp
+    std::vector<Vec3> vertexPosition;
+    std::vector<Vec3> vertexNormal;
+    std::vector<Vec2> uvTexCoords;
+    // Complete
+    std::vector<Vertex_PCUTBN> vertices;
+};
+
+class ModelLoader : public ResourceLoader<FMesh>
+{
+protected:
+    Renderer* m_renderer; // Cached renderer backend
+
+public:
+    ModelLoader(Renderer* renderer) : m_renderer(renderer)
+    {
+    }
+
+    virtual ~ModelLoader() = default;
+};
