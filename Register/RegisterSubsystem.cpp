d465db9b6ad705d5e5c0c77cbb042102cee8c1c7
CaIzii <2470159730@qq.com>
2025-07-15 07:42:21 -0500

    feat(engine): Add `RegisterSubsystem` with header file for game object registry mechanism

---
+++ Register/RegisterSubsystem.cpp
@@ -0,0 +1,1 @@
+﻿#include "RegisterSubsystem.hpp"
---
+++ Register/RegisterSubsystem.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+/**
+ * @class RegisterSubsystem
+ *
+ * @brief The RegisterSubsystem class is responsible for the registry mechanism of game object types,
+ * modeled similarly to NeoForge's Registry/DeferredRegister.
+ *
+ * This class serves as the foundational subsystem for managing registrations
+ * of various game object types in a structured and extensible manner.
+ *
+ * Users may utilize this class to implement and manage the process of registering
+ * game object types, ensuring that they are recognized and properly instantiated
+ * within the encompassing game framework.
+ */
+class RegisterSubsystem
+{
+public:
+};
