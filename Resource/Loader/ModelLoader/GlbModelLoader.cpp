57db6eb82fd7a4de8e80956689cda1b2945fe013
CaIzii <2470159730@qq.com>
2025-07-15 03:30:03 -0500

    feat(renderer): Add `GlbModelLoader` for GLB file support in model loading
    - Introduced `GlbModelLoader` class with `CanLoad`, `Load`, `GetPriority`, and `GetLoaderName` methods.
    - Initial implementation supports basic file loading and metadata checks (TODO for extension validation).

---
+++ Resource/Loader/ModelLoader/GlbModelLoader.cpp
@@ -0,0 +1,21 @@
+﻿#include "GlbModelLoader.hpp"
+
+#include "Engine/Core/EngineCommon.hpp"
+
+GlbModelLoader::GlbModelLoader(IRenderer* renderer) : ModelLoader(renderer)
+{
+}
+
+bool GlbModelLoader::CanLoad(const std::string& extension) const
+{
+    UNUSED(extension)
+    // TODO: Check whether or not have meta file and extension matches
+    return true;
+}
+
+std::unique_ptr<FMesh> GlbModelLoader::Load(const ResourceLocation& location, const std::string& filePath)
+{
+    FMesh mesh;
+
+    return std::make_unique<FMesh>(mesh);
+}
---
+++ Resource/Loader/ModelLoader/GlbModelLoader.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "ModelLoader.hpp"
+
+class GlbModelLoader : public ModelLoader
+{
+public:
+    GlbModelLoader(IRenderer* renderer);
+
+    bool                   CanLoad(const std::string& extension) const override;
+    std::unique_ptr<FMesh> Load(const ResourceLocation& location, const std::string& filePath) override;
+    int                    GetPriority() const override { return 101; }
+    std::string            GetLoaderName() const override { return "GlbModelLoader"; }
+
+private:
+};
