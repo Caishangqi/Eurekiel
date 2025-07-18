868b125c2c6e2b0a9e0f2a9cce8908eac9cef48d
CaIzii <2470159730@qq.com>
2025-07-18 04:57:37 -0500

    feat(renderer): Add `RenderTarget` class for DirectX resource management
    - Introduced `RenderTarget` class to encapsulate DirectX texture, RTV, SRV, and dimensions.
    - Implemented resource cleanup in `~RenderTarget` and `Release` methods.
    - Updated project file to include `RenderTarget` sources in the build.

---
+++ Renderer/RenderTarget.cpp
@@ -0,0 +1,25 @@
+﻿#include "RenderTarget.hpp"
+
+RenderTarget::~RenderTarget()
+{
+    Release();
+}
+
+void RenderTarget::Release()
+{
+    if (srv)
+    {
+        srv->Release();
+        srv = nullptr;
+    }
+    if (rtv)
+    {
+        rtv->Release();
+        rtv = nullptr;
+    }
+    if (texture)
+    {
+        texture->Release();
+        texture = nullptr;
+    }
+}
---
+++ Renderer/RenderTarget.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Engine/Math/IntVec2.hpp"
+#include <d3d11.h>
+
+/**
+ * @class RenderTarget
+ *
+ * @brief Represents a rendering target in DirectX, comprising a texture, render target view,
+ *        shader resource view, and dimensions.
+ *
+ * The RenderTarget class is used to manage resources required for rendering in a DirectX
+ * environment. It encapsulates a texture, render target view (RTV), shader resource view (SRV),
+ * and their associated dimensions. The class ensures proper cleanup of DirectX resources
+ * when the object is destroyed or released.
+ */
+struct RenderTarget
+{
+public:
+    ~RenderTarget();
+
+    void Release();
+
+public:
+    ID3D11Texture2D*          texture = nullptr;
+    ID3D11RenderTargetView*   rtv     = nullptr;
+    ID3D11ShaderResourceView* srv     = nullptr;
+    IntVec2                   dimensions;
+};
