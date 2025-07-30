7bf152d1ae9c2c55daef7137eb59065c6b5a97d6
CaIzii <2470159730@qq.com>
2025-07-30 04:48:55 -0500

    chore(engine): Add `RegisterLoader` method to `LoaderRegistry` for loader management

--- Code/Engine/Resource/Loader/ResourceLoader.cpp
+++ Code/Engine/Resource/Loader/ResourceLoader.cpp
@@ -5,3 +5,10 @@
 #include "Engine/Resource/ResourceCommon.hpp"
 
 using namespace enigma::resource;
+
+void LoaderRegistry::RegisterLoader(ResourceLoaderPtr loader)
+{
+    if (!loader) return;
+
+    m_loadersByName[loader->GetLoaderName()] = loader; // Register by name
+}
