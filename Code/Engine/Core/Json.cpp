ada9607a1e719c422e01587a49cecd6b0c291f2c
CaIzii <2470159730@qq.com>
2025-07-31 19:05:18 -0500

    chore(engine): Add `JsonObject` and `JsonArray` wrappers with `JsonException` class

---
+++ Code/Engine/Core/Json.cpp
@@ -0,0 +1,7 @@
+﻿#include "Json.hpp"
+
+using namespace enigma::core;
+
+JsonException::JsonException(const std::string& message) : std::runtime_error("JsonException: " + message)
+{
+}
---
+++ Code/Engine/Core/Json.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <ThirdParty/json/json.hpp>
+#include <string>
+#include <vector>
+#include <optional>
+#include <memory>
+
+namespace enigma::core
+{
+    using Json = nlohmann::json;
+
+    /**
+     * JsonObject - JSON object wrapper similar to Java's JSONObject
+     * Provides a convenient API for JSON manipulation
+     */
+    class JsonObject
+    {
+    };
+
+    /**
+     * JsonArray - JSON array wrapper
+     * Provides a convenient API for manipulating JSON arrays
+     */
+    class JsonArray
+    {
+    public:
+    };
+
+    class JsonException : public std::runtime_error
+    {
+    public:
+        explicit JsonException(const std::string& message);
+    };
+}
