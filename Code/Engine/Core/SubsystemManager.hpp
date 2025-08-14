10318776b7f4acc59fa3dda206d8543f54582fe2
CaIzii <2470159730@qq.com>
2025-08-14 18:34:46 -0500

    chore(engine): Add `SubsystemManager` and `EngineSubsystem` interface for subsystem lifecycle management
    - Introduce `EngineSubsystem` interface with lifecycle methods (`Startup`, `Shutdown`, `BeginFrame`, `Update`, `EndFrame`) and naming convention.
    - Add `SubsystemManager` header and implementation to manage engine subsystems.

---
+++ Code/Engine/Core/SubsystemManager.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+#pragma once
+#include <string>
+
+namespace enigma::core
+{
+    class EngineSubsystem
+    {
+    public:
+        virtual             ~EngineSubsystem() = default;
+        virtual void        Startup() = 0;
+        virtual void        Shutdown() = 0;
+        virtual std::string GetSubsystemName() const = 0;
+
+        // Lifecycle methods
+        virtual void BeginFrame()
+        {
+        }
+
+        virtual void Update(float deltaTime)
+        {
+            (void)deltaTime;
+        }
+
+        virtual void EndFrame()
+        {
+        }
+    };
+
+    class SubsystemManager
+    {
+    public:
+    };
+}
