4e4abbc97d76a4074e646e35d4ac16fa2b90a1e6
CaIzii <2470159730@qq.com>
2025-08-19 11:58:39 -0500

    chore(engine): Add `ConsoleSubsystem` forward declaration and global accessor

--- Code/Engine/Core/EngineCommon.hpp
+++ Code/Engine/Core/EngineCommon.hpp
@@ -6,10 +6,16 @@
 #include "NamedStrings.hpp"
 #include "ErrorWarningAssert.hpp"
 
-extern NamedStrings          g_gameConfigBlackboard;
-extern EventSystem*          g_theEventSystem;
-extern DevConsole*           g_theDevConsole;
-extern enigma::core::Engine* g_theEngine;
+// Forward declarations
+namespace enigma::core {
+    class ConsoleSubsystem;
+}
+
+extern NamedStrings                        g_gameConfigBlackboard;
+extern EventSystem*                        g_theEventSystem;
+extern DevConsole*                         g_theDevConsole;
+extern enigma::core::Engine*               g_theEngine;
+extern enigma::core::ConsoleSubsystem*     g_theConsole;
 
 #define UNUSED(x) (void)(x);
 #define STATIC static
