497a2df5dd7e087d0ba687c15fe059b0365266cb
CaIzii <2470159730@qq.com>
2025-09-01 22:34:24 -0500

    chore(engine): Add RenderAll placeholder method to ChunkMesh

--- Code/Engine/Voxel/Chunk/ChunkMesh.cpp
+++ Code/Engine/Voxel/Chunk/ChunkMesh.cpp
@@ -1,1 +1,9 @@
 ﻿#include "ChunkMesh.hpp"
+
+#include "Engine/Core/EngineCommon.hpp"
+using namespace enigma::voxel::chunk;
+
+void ChunkMesh::RenderAll(IRenderer* renderer)
+{
+    UNUSED(renderer)
+}
