368224deb25be783ad9ba4a471535ff6b5c92774
CaIzii <2470159730@qq.com>
2025-09-18 11:18:17 -0500

    **feat(voxel): Add BlockAir class as a lightweight no-op block**
    - Introduce `BlockAir` class inheriting from `Block` for handling air blocks in voxel systems.
    - Implement methods: `OnPlaced`, `OnBroken`, `OnNeighborChanged`, `GetModelPath`, and `InitializeState` with default no-op behavior.
    - Provide registry name and namespace initialization for `BlockAir` instances.

---
+++ Code/Engine/Voxel/Builtin/BlockAir.cpp
@@ -0,0 +1,45 @@
+﻿#include "BlockAir.hpp"
+using namespace enigma::voxel::block;
+
+BlockAir::BlockAir(const std::string& registryName = "enigma", const std::string& namespaceName = "air") : Block(registryName, namespaceName)
+{
+}
+
+BlockAir::~BlockAir()
+{
+}
+
+const std::string& BlockAir::GetRegistryName() const
+{
+    return Block::GetRegistryName();
+}
+
+const std::string& BlockAir::GetNamespace() const
+{
+    return Block::GetNamespace();
+}
+
+void BlockAir::OnPlaced(enigma::voxel::world::World* world, const enigma::voxel::block::BlockPos& pos, enigma::voxel::block::BlockState* state)
+{
+    Block::OnPlaced(world, pos, state);
+}
+
+void BlockAir::OnBroken(enigma::voxel::world::World* world, const enigma::voxel::block::BlockPos& pos, enigma::voxel::block::BlockState* state)
+{
+    Block::OnBroken(world, pos, state);
+}
+
+void BlockAir::OnNeighborChanged(enigma::voxel::world::World* world, const enigma::voxel::block::BlockPos& pos, enigma::voxel::block::BlockState* state, Block* neighborBlock)
+{
+    Block::OnNeighborChanged(world, pos, state, neighborBlock);
+}
+
+std::string BlockAir::GetModelPath(enigma::voxel::block::BlockState* state) const
+{
+    return Block::GetModelPath(state);
+}
+
+void BlockAir::InitializeState(enigma::voxel::block::BlockState* state, const property::PropertyMap& properties)
+{
+    Block::InitializeState(state, properties);
+}
---
+++ Code/Engine/Voxel/Builtin/BlockAir.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Engine/Registry/Block/Block.hpp"
+
+namespace enigma::voxel::block
+{
+    class BlockAir : public registry::block::Block
+    {
+    public:
+        BlockAir(const std::string& registryName, const std::string& namespaceName);
+        ~BlockAir() override;
+        const std::string& GetRegistryName() const override;
+        const std::string& GetNamespace() const override;
+        void               OnPlaced(enigma::voxel::world::World* world, const enigma::voxel::block::BlockPos& pos, enigma::voxel::block::BlockState* state) override;
+        void               OnBroken(enigma::voxel::world::World* world, const enigma::voxel::block::BlockPos& pos, enigma::voxel::block::BlockState* state) override;
+        void               OnNeighborChanged(enigma::voxel::world::World* world, const enigma::voxel::block::BlockPos& pos, enigma::voxel::block::BlockState* state, Block* neighborBlock) override;
+        std::string        GetModelPath(enigma::voxel::block::BlockState* state) const override;
+
+    protected:
+        void InitializeState(enigma::voxel::block::BlockState* state, const property::PropertyMap& properties) override;
+    };
+}
