1c2b1794720b82830e7e183ae52f3c70c92b54cc
CaIzii <2470159730@qq.com>
2025-09-19 20:45:21 -0500

    **feat(voxel): Add DefaultBlock header and implementation files**
    - Introduce `DefaultBlock.hpp` and `DefaultBlock.cpp` for defining default block types.
    - Add an inline `AIR` block as a placeholder in the voxel namespace.
    
    Signed-off-by: CaIzii <2470159730@qq.com>

---
+++ Code/Engine/Voxel/Builtin/DefaultBlock.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <memory>
+
+namespace enigma::registry::block
+{
+    class Block;
+}
+
+namespace enigma::voxel::block
+{
+    inline std::shared_ptr<registry::block::Block> AIR = nullptr;
+}
