fbd6b79e0e3e97709dcc5a5a2e9c519a5dbc2656
CaIzii <2470159730@qq.com>
2025-10-11 01:00:55 -0500

    **refactor(graphic): Deprecate obsolete framebuffer and bindless resource types**
    Removes `D12Framebuffer`, `ShadowRenderer`, `RenderTargets` classes, and `BindlessResourceType` enum. These components are marked as obsolete due to architectural alignment with Bindless-MRT goals. Streamlines the codebase to reduce maintenance complexity and focus on updated rendering features.
    
    Signed-off-by: CaIzii <2470159730@qq.com>

--- Code/Engine/Graphic/Resource/RenderTarget/D12RenderTarget.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "D12RenderTarget.hpp"
-using namespace enigma::graphic;
-
-BindlessResourceType D12RenderTarget::GetDefaultBindlessResourceType() const
-{
-    // RenderTarget作为可采样纹理资源使用Texture2D类型
-    // 对应Iris中将RenderTarget主纹理绑定为GL_TEXTURE_2D供着色器采样
-    return BindlessResourceType::Texture2D;
-}
