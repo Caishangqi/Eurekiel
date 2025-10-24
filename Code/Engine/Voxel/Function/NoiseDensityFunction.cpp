93988d77e69537d1a2f543b3d377b7450ba0deab
CaIzii <2470159730@qq.com>
2025-10-24 11:02:44 -0500

    **feat(engine): Implement NoiseDensityFunction and update project structure**
    Added `NoiseDensityFunction` with scaling support for evaluating voxel density using `NoiseGenerator`. Included the implementation in source and header files, and updated the project to reference `TerrainShaper` for enhanced modularity in terrain generation.
    
    Signed-off-by: CaIzii <2470159730@qq.com>

--- Code/Engine/Voxel/Function/NoiseDensityFunction.cpp
+++ Code/Engine/Voxel/Function/NoiseDensityFunction.cpp
@@ -1,2 +1,26 @@
 ﻿#include "NoiseDensityFunction.hpp"
+
+#include "Engine/Voxel/NoiseGenerator/NoiseGenerator.hpp"
 using namespace enigma::voxel;
+
+NoiseDensityFunction::NoiseDensityFunction(std::shared_ptr<NoiseGenerator> noise, float xzScale, float yScale) : m_noise(noise),
+                                                                                                                 m_xzScale(xzScale),
+                                                                                                                 m_yScale(yScale)
+{
+}
+
+float NoiseDensityFunction::Evaluate(int x, int y, int z) const
+{
+    //Apply additional scaling
+    float scaledX = (float)x * m_xzScale;
+    float scaledY = (float)y * m_yScale;
+    float scaledZ = (float)z * m_xzScale;
+
+    //Call the Sample method of NoiseGenerator
+    return m_noise->Sample(scaledX, scaledY, scaledZ);
+}
+
+std::string NoiseDensityFunction::GetTypeName() const
+{
+    return "engine:noise";
+}
--- Code/Engine/Voxel/Function/NoiseDensityFunction.hpp
+++ Code/Engine/Voxel/Function/NoiseDensityFunction.hpp
@@ -3,8 +3,25 @@
 
 namespace enigma::voxel
 {
+    class NoiseGenerator;
+
     class NoiseDensityFunction : public DensityFunction
     {
     public:
+        NoiseDensityFunction(
+            std::shared_ptr<NoiseGenerator> noise,
+            float                           xzScale = 1.0f,
+            float                           yScale  = 1.0f
+        );
+
+        float Evaluate(int x, int y, int z) const override;
+        //static std::unique_ptr<NoiseDensityFunction> FromJson( const Json&    json, NoiseRegistry& noiseRegistry);
+
+        std::string GetTypeName() const override;
+
+    private:
+        std::shared_ptr<NoiseGenerator> m_noise;
+        float                           m_xzScale; // Additional scaling of the XZ plane
+        float                           m_yScale; // Additional Y-axis scaling
     };
 }
