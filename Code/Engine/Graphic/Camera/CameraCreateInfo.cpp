d2a91f73ce2e0b88b87a89f2b8f20d7ac78e9646
CaIzii <2470159730@qq.com>
2025-11-01 02:26:08 -0500

    **feat(graphic): Add CameraCreateInfo structure for unified camera configuration**
    Introduced `CameraCreateInfo` class to consolidate camera creation and configuration. Added support for perspective, orthographic, and 2D UI camera creation with predefined parameter defaults. Includes customizable position, orientation, projection modes, and viewports for streamlined camera setup in the graphics engine.
    
    Signed-off-by: CaIzii <2470159730@qq.com>

---
+++ Code/Engine/Graphic/Camera/CameraCreateInfo.cpp
@@ -0,0 +1,43 @@
+﻿#include "CameraCreateInfo.hpp"
+using namespace enigma::graphic;
+
+CameraCreateInfo CameraCreateInfo::CreatePerspective(const Vec3& pos, const EulerAngles& orient, float aspect, float fov, float nearPlane, float farPlane)
+{
+    CameraCreateInfo info;
+    info.mode              = CameraMode::Perspective;
+    info.position          = pos;
+    info.orientation       = orient;
+    info.perspectiveAspect = aspect;
+    info.perspectiveFOV    = fov;
+    info.perspectiveNear   = nearPlane;
+    info.perspectiveFar    = farPlane;
+    return info;
+}
+
+CameraCreateInfo CameraCreateInfo::CreateOrthographic(const Vec3& pos, const EulerAngles& orient, const Vec2& bottomLeft, const Vec2& topRight, float nearPlane, float farPlane)
+
+{
+    CameraCreateInfo info;
+    info.mode                   = CameraMode::Orthographic;
+    info.position               = pos;
+    info.orientation            = orient;
+    info.orthographicBottomLeft = bottomLeft;
+    info.orthographicTopRight   = topRight;
+    info.orthographicNear       = nearPlane;
+    info.orthographicFar        = farPlane;
+    return info;
+}
+
+CameraCreateInfo CameraCreateInfo::CreateUI2D(const Vec2& screenSize, float nearPlane, float farPlane)
+
+{
+    CameraCreateInfo info;
+    info.mode                   = CameraMode::Orthographic;
+    info.position               = Vec3::ZERO;
+    info.orientation            = EulerAngles::ZERO;
+    info.orthographicBottomLeft = Vec2::ZERO;
+    info.orthographicTopRight   = screenSize;
+    info.orthographicNear       = nearPlane;
+    info.orthographicFar        = farPlane;
+    return info;
+}
