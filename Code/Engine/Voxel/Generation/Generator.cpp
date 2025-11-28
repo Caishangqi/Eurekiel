87094ef1866b5df7ab86c844674c6ea81b4cf2c5
CaIzii <2470159730@qq.com>
2025-11-28 00:28:37 -0600

    Revert "feat(Voxel): add chunk management and generation system"
    This reverts commit 1e64c5fb8aed22d365e52db84e863f181e62e825.

--- Code/Engine/Voxel/Generation/Generator.cpp
+++
@@ -1,8 +0,0 @@
-#include "Generator.hpp"
-#include "../../Core/Logger/LoggerAPI.hpp"
-
-namespace enigma::voxel
-{
-    // No additional implementation needed for now
-    // All methods are either pure virtual or have inline implementations in the header
-}
