618ad992fbaf7e62558af28d6ba4b0c9bb856f13
CaIzii <2470159730@qq.com>
2026-01-07 00:29:52 -0600

    Add new files for common definitions and log category for RenderTarget.
    Signed-off-by: CaIzii <2470159730@qq.com>

---
+++ Code/Engine/Graphic/Target/RenderTargetProviderCommon.cpp
@@ -0,0 +1,15 @@
+#include "RenderTargetProviderCommon.hpp"
+
+// ============================================================================
+// RenderTargetProviderCommon.cpp - [NEW] Log category definition
+// Part of RenderTarget Manager Unified Architecture Refactoring
+// ============================================================================
+
+namespace enigma::graphic
+{
+    // ========================================================================
+    // Log Category Definition
+    // ========================================================================
+
+    DEFINE_LOG_CATEGORY(LogRenderTargetProvider);
+} // namespace enigma::graphic
---
+++ Code/Engine/Graphic/Target/RenderTargetProviderCommon.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// ============================================================================
+// RenderTargetProviderCommon.hpp - [NEW] Common definitions for RenderTarget Provider module
+// Part of RenderTarget Manager Unified Architecture Refactoring
+// ============================================================================
+
+#include "Engine/Core/LogCategory/LogCategory.hpp"
+
+namespace enigma::graphic
+{
+    // ========================================================================
+    // Log Category Declaration
+    // ========================================================================
+
+    DECLARE_LOG_CATEGORY_EXTERN(LogRenderTargetProvider);
+} // namespace enigma::graphic
