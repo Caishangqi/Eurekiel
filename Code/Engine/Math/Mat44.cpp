173d5a5cbaa1b94a5d32b239dee2f2f6b07530af
CaIzii <2470159730@qq.com>
2026-01-24 11:21:50 -0600

    Replace orthonormal inverse with general 4x4 matrix inverse using Cramer's rule
    Signed-off-by: CaIzii <2470159730@qq.com>

--- Code/Engine/Math/Mat44.cpp
+++ Code/Engine/Math/Mat44.cpp
@@ -351,7 +351,57 @@ const Mat44 Mat44::GetOrthonormalInverse() const
 
 const Mat44 Mat44::GetInverse() const
 {
-    return GetOrthonormalInverse();
+    // [FIX] General 4x4 matrix inverse using Cramer's rule with cofactors
+    // Works for ALL matrices including perspective projection
+    float a00 = m_values[0],  a01 = m_values[1],  a02 = m_values[2],  a03 = m_values[3];
+    float a10 = m_values[4],  a11 = m_values[5],  a12 = m_values[6],  a13 = m_values[7];
+    float a20 = m_values[8],  a21 = m_values[9],  a22 = m_values[10], a23 = m_values[11];
+    float a30 = m_values[12], a31 = m_values[13], a32 = m_values[14], a33 = m_values[15];
+
+    // Calculate 2x2 determinants
+    float b00 = a00 * a11 - a01 * a10;
+    float b01 = a00 * a12 - a02 * a10;
+    float b02 = a00 * a13 - a03 * a10;
+    float b03 = a01 * a12 - a02 * a11;
+    float b04 = a01 * a13 - a03 * a11;
+    float b05 = a02 * a13 - a03 * a12;
+    float b06 = a20 * a31 - a21 * a30;
+    float b07 = a20 * a32 - a22 * a30;
+    float b08 = a20 * a33 - a23 * a30;
+    float b09 = a21 * a32 - a22 * a31;
+    float b10 = a21 * a33 - a23 * a31;
+    float b11 = a22 * a33 - a23 * a32;
+
+    // Calculate determinant
+    float det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+
+    if (fabsf(det) < 1e-10f)
+    {
+        // Singular matrix, return identity as fallback
+        return Mat44::IDENTITY;
+    }
+
+    float invDet = 1.0f / det;
+
+    Mat44 inv;
+    inv.m_values[0]  = (a11 * b11 - a12 * b10 + a13 * b09) * invDet;
+    inv.m_values[1]  = (-a01 * b11 + a02 * b10 - a03 * b09) * invDet;
+    inv.m_values[2]  = (a31 * b05 - a32 * b04 + a33 * b03) * invDet;
+    inv.m_values[3]  = (-a21 * b05 + a22 * b04 - a23 * b03) * invDet;
+    inv.m_values[4]  = (-a10 * b11 + a12 * b08 - a13 * b07) * invDet;
+    inv.m_values[5]  = (a00 * b11 - a02 * b08 + a03 * b07) * invDet;
+    inv.m_values[6]  = (-a30 * b05 + a32 * b02 - a33 * b01) * invDet;
+    inv.m_values[7]  = (a20 * b05 - a22 * b02 + a23 * b01) * invDet;
+    inv.m_values[8]  = (a10 * b10 - a11 * b08 + a13 * b06) * invDet;
+    inv.m_values[9]  = (-a00 * b10 + a01 * b08 - a03 * b06) * invDet;
+    inv.m_values[10] = (a30 * b04 - a31 * b02 + a33 * b00) * invDet;
+    inv.m_values[11] = (-a20 * b04 + a21 * b02 - a23 * b00) * invDet;
+    inv.m_values[12] = (-a10 * b09 + a11 * b07 - a12 * b06) * invDet;
+    inv.m_values[13] = (a00 * b09 - a01 * b07 + a02 * b06) * invDet;
+    inv.m_values[14] = (-a30 * b03 + a31 * b01 - a32 * b00) * invDet;
+    inv.m_values[15] = (a20 * b03 - a21 * b01 + a22 * b00) * invDet;
+
+    return inv;
 }
 
 const Mat44 Mat44::GetTranspose()
