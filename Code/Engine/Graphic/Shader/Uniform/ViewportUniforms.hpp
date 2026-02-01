29c9ef04e95670359059493599f18aef3b72176c
CaIzii <2470159730@qq.com>
2026-01-31 23:15:19 -0600

    Add ViewportUniforms struct for shader viewport data management
    Signed-off-by: CaIzii <2470159730@qq.com>

---
+++ Code/Engine/Graphic/Shader/Uniform/ViewportUniforms.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+namespace enigma::graphic
+{
+#pragma warning(push)
+#pragma warning(disable: 4324)
+    struct ViewportUniforms
+    {
+        float viewHeight  = 0.f;
+        float viewWidth   = 0.f;
+        float aspectRatio = 0.f;
+    };
+}
